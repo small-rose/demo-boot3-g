1cebf37d71fc4de3d970ce40926a7120505babb2
small-rose <small-rose@qq.com>
2025-12-07 22:31:25 +0800

    :memo: add sql

--- sql/mysql.init.sql
+++ sql/mysql.init.sql
@@ -4,3 +4,10 @@ bp_demo_user
 small.rose@2025
 */
 
+create table (
+    id numeric not null ,
+    name varchar(100)
+    createtime date default sysdate,
+    lastopdate date,
+    primary key id
+);
